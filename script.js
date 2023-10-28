c624674ac3f0ccd42ffef9aab3232b4a1d0c545e
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-10-28 12:44:51 +0200

    feat: lazy loading images

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
+const imgTargets = document.querySelectorAll('img[data-src]');
 
 const openModal = e => {
   e.preventDefault();
@@ -108,22 +109,45 @@ headerObserver.observe(header);
 
 // reveal sections
 const revealSection = (entries, observer) => {
-  const [entry] = entries
+  const [entry] = entries;
 
-  if(!entry.isIntersecting) return
-  entry.target.classList.remove('section--hidden')
-  observer.unobserve(entry.target)
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.15
+  threshold: 0.15,
 });
 
-allSections.forEach((section) => {
-  sectionObserver.observe(section)
-  section.classList.add('section--hidden')
-})
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// lazy loading images
+const loadImg = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ///////////////////////////////////////////////////////////////////////
 // console.log(document.documentElement);
