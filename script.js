84841b499f9d3a660280e8e62f9358f7bd916beb
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-10-28 12:07:58 +0200

    feat: sticky nav

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header')
 
 const openModal = e => {
   e.preventDefault();
@@ -87,6 +88,23 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// sticky navigation
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = (entries) => {
+  const [entry] = entries
+  console.log(entry);
+  if(!entry.isIntersecting) nav.classList.add('sticky')
+  else nav.classList.remove('sticky')
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+})
+headerObserver.observe(header)
 
 ///////////////////////////////////////////////////////////////////////
 // console.log(document.documentElement);
